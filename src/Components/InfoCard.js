ff502e9aba746b3c7d40a74b1d439a49fbab2565
Crystal Martin <drcrystalm@gmail.com>
2019-10-10 18:23:49 -0500

    used Bootstrap to style a card

--- src/Components/InfoCard.js
+++ src/Components/InfoCard.js
@@ -1,16 +1,30 @@
 import React from "react"
+import {
+    Card,
+    CardImg,
+    CardText,
+    CardBody,
+    CardTitle,
+    CardSubtitle,
+    Button,
+    CardFooter,
+} from "reactstrap"
 
 const InfoCard = props => {
     return (
-        <div className='data-list' key={props.index}>
-            <img alt='' src={props.photo} />
-
-            <h2>Title: {props.title}</h2>
-            <p>{props.media_type}</p>
-            <p>Date: {props.date}</p>
-            {/* <p>This looks important => {props.hdurl}</p> */}
-            <p>{props.explanation}</p>
-        </div>
+        <Card className='try' key={props.index}>
+            <CardBody className='try'>
+                <Button color='warning'>Hit Me</Button>
+                <CardImg src={props.imgUrl} alt='' />
+                <CardTitle className='text-primary'>{props.title}</CardTitle>
+                {/* <CardText>{props.description}</CardText> */}
+                <CardSubtitle>Date: {props.date} </CardSubtitle>
+                <CardSubtitle className='text-info'>
+                    {props.explanation}
+                </CardSubtitle>
+            </CardBody>
+            <CardFooter>Made By Crystal Martin</CardFooter>
+        </Card>
     )
 }
 
